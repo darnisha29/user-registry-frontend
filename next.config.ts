0ef06735391f4ecd532c3e442ac78493bb589a9b
Rushik Ghuntala <rushik.ghuntala@propelius.tech>
2024-12-21 15:25:34 +0530

    Modified next config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  output: 'export',  // This configures the app for static export
   reactStrictMode: true,  
 };
 
